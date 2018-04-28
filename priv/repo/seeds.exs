4787c19626a7c0fb9c63a7a1664bd1791ef3fe7b
Caio Sym <caio.sym@climate.com>
2018-04-28 15:24:36 -0300

    Add more seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -15,3 +15,11 @@ alias CaioNotes.Repo
 
 %Note{contents: "I'm a happy note", color: "#FF00FF"} |> Repo.insert!
 %Note{contents: "I'm another note", color: "#0000FF"} |> Repo.insert!
+%Note{contents: "I'm another note 2", color: "#0000FF"} |> Repo.insert!
+%Note{contents: "I'm another note 3", color: "#0000FF"} |> Repo.insert!
+%Note{contents: "I'm another note 4", color: "#0000FF"} |> Repo.insert!
+%Note{contents: "I'm another note 5", color: "#0000FF"} |> Repo.insert!
+%Note{contents: "I'm another note 6", color: "#0000FF"} |> Repo.insert!
+%Note{contents: "I'm another note 7", color: "#0000FF"} |> Repo.insert!
+%Note{contents: "I'm another note 8", color: "#0000FF"} |> Repo.insert!
+%Note{contents: "I'm another note 9", color: "#0000FF"} |> Repo.insert!
